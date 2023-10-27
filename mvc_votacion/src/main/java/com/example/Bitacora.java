67e91ebcfafbd5b89e5db6f831a56ab6bd5a181c
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-10-27 02:33:57 -0600

    project version updated
    se agregó la funcionalidad de bitácora

--- mvc_votacion/src/main/java/com/example/Bitacora.java
+++ mvc_votacion/src/main/java/com/example/Bitacora.java
@@ -1,19 +1,46 @@
 package com.example;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Bitacora {
-    public static void registrarAccion(String clase, String mensaje) {
+
+    private String nombreArchivo;
+    private SimpleDateFormat formatoFecha;
+
+    public Bitacora(){
+        this.nombreArchivo = "bitacora.txt";
+        this.formatoFecha = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+
+    public void crearArchivo() {
         try {
-            LocalDateTime ahora = LocalDateTime.now();
-            String registro = ahora + " - " + clase + " - " + mensaje + "\n";
-            Files.write(Paths.get("bitacora.txt"), registro.getBytes(), StandardOpenOption.APPEND);
+            File archivo = new File(nombreArchivo);
+            if (archivo.createNewFile()) {
+                System.out.println("Archivo creado: " + archivo.getName());
+            } else {
+                System.out.println("Archivo modificado: " + archivo.getName());
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error al crear el archivo: " + e.getMessage());
         }
     }
+
+    public void escribirArchivo(String texto){
+        try {
+            FileWriter archivo = new FileWriter(nombreArchivo, true);
+            try (BufferedWriter bw = new BufferedWriter(archivo)) {
+                String fecha = formatoFecha.format(new Date());
+                bw.write(texto + " " + fecha); 
+                bw.newLine(); 
+            }
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+        }       
+    }
+    
 }
--- mvc_votacion/src/main/java/com/example/Controlador.java
+++ mvc_votacion/src/main/java/com/example/Controlador.java
@@ -6,10 +6,13 @@ import java.awt.event.*;
 public class Controlador {
     private Vista vista;
     private ArrayList<Producto> productos;
+    private Bitacora bitacora;
 
     public Controlador(ArrayList<Producto> productos, Vista vista) {
         this.productos = productos;
         this.vista = vista;
+        this.bitacora = new Bitacora();
+        bitacora.crearArchivo();
 
         vista.addVotoListenerBtn1(new votoListener(0));
         vista.addVotoListenerBtn2(new votoListener(1));
@@ -25,7 +28,11 @@ public class Controlador {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            productos.get(this.tipoBtn).votar();
+            //productos.get(this.tipoBtn).votar();
+            votarPorProducto(this.tipoBtn);
+            Producto producto = productos.get(this.tipoBtn);
+            String texto = "Se ha actualizado la grafica para " + producto.getNombre() + " en el metodo actionPerfomed";
+            bitacora.escribirArchivo(texto);
             switch (this.tipoBtn) {
                 case 0:
                     vista.actualizarDatosGraficos(productos);
@@ -44,6 +51,7 @@ public class Controlador {
     public void votarPorProducto(int indice) {
         Producto producto = productos.get(indice);
         producto.votar();
-        // Registra la acción en la bitácora
+        String texto = "Se ha votado por el producto " + producto.getNombre() + " en el metodo votarPorProducto()";
+        bitacora.escribirArchivo(texto);
     }
 }
